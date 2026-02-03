473c07a342b0b2c037667744aad3f619622f0ca1
dan-acsc <dan-acsc@users.noreply.github.com>
2026-02-03 02:28:44 +0000

    feat: remove black

--- azul_restapi_server/security/no_auth.py
+++ azul_restapi_server/security/no_auth.py
@@ -14,7 +14,7 @@ def validate_token(request: Request, token: str = Depends(lambda: None)) -> User
     request.state.user_info = oidc_shared.claims_to_user(
         {
             "exp": -1,
-            "token_type": "Bearer",  # nosec B105
+            "token_type": "Bearer",  # noqa S105
             "preferred_username": "anony-moose",
             "org": "testing",
             "roles": ["validated"],
