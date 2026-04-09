0120b0765c8daa1f6a8a00eceda7ad808bab1b56
Matthew McDonald <matthew.mcdonald@ACSC.cloud>
2026-04-09 03:27:48 +0000

    fix tests

--- azul_restapi_server/settings.py
+++ azul_restapi_server/settings.py
@@ -60,8 +60,8 @@ class RetrohuntConfig(BaseSettings):
     cleanup_running_delay: int = 0
     ttl: int = 0
     exception_wait: int = 0
-    username: str = Field(env="REDIS_USERNAME")
-    password: str = Field(env="REDIS_PASSWORD")
+    username: str = Field(default="", env="REDIS_USERNAME")
+    password: str = Field(default="", env="REDIS_PASSWORD")
 
     model_config = SettingsConfigDict(env_prefix="RETROHUNT_")
 
